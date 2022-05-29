bba7486a052475b3427dbee97203e73c2342992d
AndrewSigorskih <crescent8547@gmail.com>
2022-05-30 00:42:16 +0300

    fuseTrees.c cleaned up

--- source/fuseTrees.c
+++ source/fuseTrees.c
@@ -6,7 +6,7 @@ int* randomArgMax(double** table, int n)
     double maxVal;
     int** maxIdx;
     int* res;
-    //printf("randomArgMax started\n");
+
     maxVal = 0;
     maxNum = 0;
     for (i = 0; i < n; i++)
@@ -23,7 +23,7 @@ int* randomArgMax(double** table, int n)
 
         }
     }
-    //printf("max values to choose from: %d\n", maxNum);
+
     maxIdx = (int**)calloc(maxNum, sizeof(int*));
 	for (i = 0; i < maxNum; i++)
 	{
@@ -35,10 +35,8 @@ int* randomArgMax(double** table, int n)
     {
         for (j = 0; j < n; j++)
         {
-            //if (table[i][j] == maxVal)
             if (fabs(maxVal - table[i][j]) <= 1.192093e-06)
             {
-                //printf("equality works: %f %f\n", table[i][j], maxVal);
                 maxIdx[k][0] = i;
                 maxIdx[k][1] = j;
                 k++;
@@ -52,9 +50,7 @@ int* randomArgMax(double** table, int n)
 		res[0] = maxIdx[0][0];
 		res[1] = maxIdx[0][1];
 	} else {
-        //srand(time(NULL));
 		j = rand() % maxNum;
-        //printf("random pos chosen: %d\n", j);
 		res[0] = maxIdx[j][0];
 		res[1] = maxIdx[j][1];
 	}
@@ -134,10 +130,8 @@ BranchArray* treeToBranchFull(Tree* tree, int* permutation)
     NodeStack* stack;
     Node* curNode = 0;
     Node* nextNode = 0;
-    //char* isTrivial;
 
     ba = branchArrayCreate(branchNum);
-    //result = branchArrayCreate(tree->nodesNum - 1);
     stack = nodeStackCreate(tree->nodesNum);
 
     treeWash(tree);
@@ -223,36 +217,9 @@ size_t* getLeavesToTransfer(Tree* reference, char* name1, char* name2, int* pair
    
     // technical branch containing only two needed leaves
     mask = branchCreate(reference->leavesNum);
-    
-    //printf("empty branch:\t");
-    //branchPrint(br);
-    /*
-    p = 1;
-    p = p << (pos1 & (intSize-1));
-    mask->branch[pos1 / intSize] |= p;
-    p = 1;
-    p = p << (pos2 & (intSize-1));
-    mask->branch[pos2 / intSize] |= p;
-    */
     branchAddLeafUnsafe(mask, pos1);
     branchAddLeafUnsafe(mask, pos2);
     branchCalculateLeavesPosNum(mask);
-
-    /*
-    printf("filled branch:\t");
-    branchPrint(mask);
-    printf("\n");
-
-    //branchNormalize(mask);
-
-    //printf("normalized branch:\t");
-    //branchPrint(mask);
-    printf("\n");
-    printf("Branch Array of size %d from tree with %d nodes:\n", ba->size, reference->nodesNum);
-    for(i = 0; i < ba->size; ++i)
-    {
-        branchPrint(ba->array[i]);
-    }*/
 
     permutation = getRange(0, reference->leavesNum);
     ba = treeToBranchFull(reference, permutation);
@@ -265,9 +232,7 @@ size_t* getLeavesToTransfer(Tree* reference, char* name1, char* name2, int* pair
     {
         branchAddLeafUnsafe(maxBranch, i); 
     }
-    //branchCalculateLeavesPosNum(maxBranch);
-    //printf("Maximum branch of size %d:\n", branchGetLeavesPosNum(maxBranch));
-    //branchPrint(maxBranch);
+
     for (i = 0; i < ba->maxSize; i++)
     {
         branchArrayAdd(revBa, branchXor(maxBranch, ba->array[i]));
@@ -276,21 +241,19 @@ size_t* getLeavesToTransfer(Tree* reference, char* name1, char* name2, int* pair
 
     j = 0;
     minSize = reference->leavesNum;
-    //printf("\n\n");
+
+    // get minimal of branches that have two selected leaves
+    // from up and down perspective
     for (i = 0; i < ba->size; i++)
     {
+        // first array
         branchCalculateLeavesPosNum(ba->array[i]);
         k = branchGetLeavesPosNum(ba->array[i]);
-        // get minimal of branches that have two selected leaves
-        //printf("k: %d\t", k);
-        //branchPrint(branchAnd(ba->array[i], mask));
 
         andBranch = branchAnd(ba->array[i], mask);
         branchCalculateLeavesPosNum(andBranch);
         if ((k < minSize) && (branchGetLeavesPosNum(andBranch) == 2))
         {
-            //printf("new branch:\t");
-            //branchPrint(ba->array[i]);
             minSize = k;
             maxBranch = ba->array[i];
 			j = i;
@@ -313,6 +276,7 @@ size_t* getLeavesToTransfer(Tree* reference, char* name1, char* name2, int* pair
 
     }
 
+    /*
     if (minSize == reference->leavesNum)
     {
         printf("failed to catch branch!\n");
@@ -327,24 +291,15 @@ size_t* getLeavesToTransfer(Tree* reference, char* name1, char* name2, int* pair
         branchPrint(rev);
         printf("with %d leaves\n", branchGetLeavesPosNum(rev));
         exit(1);
-    }
+    } */
 
-    //printf("foung good branch of size %d:  ", branchGetLeavesPosNum(maxBranch));
-    //branchPrint(maxBranch);
     // store size for later 
     pairIdx[0] = minSize;
     pairIdx[1] = reference->leavesNum - minSize;
+
     // create leaves names array
     size_t leavesPosSize = 0;
     leavesPos = branchGetLeavesPos(maxBranch, &leavesPosSize, reference->leavesNum);
-
-    /*printf("positions of leaves to transfer: ");
-    for (i = 0; i < leavesPosSize; i++)
-    {
-        printf("%ld ", leavesPos[i]);
-    }
-    printf("\n"); 
-    */
 
     branchDelete(mask);
     for(i = 0; i < ba->size; ++i)
@@ -381,62 +336,23 @@ Tree* fuseTrees(Tree* intree1, Tree* intree2)
 
 	dist1 = pairwiseLeafDistances(intree1);
 	dist2 = pairwiseLeafDistances(intree2);
-    /*
-    for (i = 0; i < intree1->leavesNum; i++)
-    { printf("%d ", permutation[i]); }
-    printf("\n");
-    for (i = 0; i < intree1->leavesNum; i++)
-    { printf("%s ", intree1->leaves[i]->name); }
-    printf("\n");
-    for (i = 0; i < intree1->leavesNum; i++)
-    { printf("%s ", intree2->leaves[i]->name); }
-    printf("\n\n");
-    printf("distance matrices done\n");
-    for (i = 0; i < intree1->leavesNum; i++)
-    {
-        printf("%s ", intree1->leaves[i]->name);
-        for (j = 0; j < intree1->leavesNum; j++)
-        { printf("%d ", dist1[i][j]);}
-        printf("\n");
-    }
-    printf("\n\n");
-    for (i = 0; i < intree1->leavesNum; i++)
-    {
-        printf("%s ", intree2->leaves[permutation[i]]->name);
-        for (j = 0; j < intree1->leavesNum; j++)
-        { printf("%d ", dist2[permutation[i]][permutation[j]]);}
-        printf("\n");
-    } 
-    printf("\n\n");
-    */
+    
 	relDist = relativeLeafDistances(dist1, dist2, permutation, intree1->leavesNum);
-    /*
-    for (i = 0; i < intree1->leavesNum; i++)
-    {
-        for (j = 0; j < intree1->leavesNum; j++)
-        { printf("%f ", relDist [i][j]);}
-        printf("\n");
-    } 
-    printf("\n\n");
-    printf("reldist done\n");
-    */
+    
 	pairIdx = randomArgMax(relDist, intree1->leavesNum);
-    //printf("paired indices chosen: %d %d\n", pairIdx[0], pairIdx[1]);
 	i = pairIdx[0];
 	j = pairIdx[1];
 	name1 = intree1->leaves[i]->name;
 	name2 = intree1->leaves[j]->name;
-    //printf("leaf names: %s %s \n", intree1->leaves[i]->name, intree1->leaves[j]->name);
+
     // decide which tree is a donor
     if (dist1[i][j] < dist2[permutation[i]][permutation[j]])
     {
         reference = treeCopy(intree1, 1);
         rugged = treeCopy(intree2, 1);
-        //printf("copied tree1\n");
     } else {
         reference = treeCopy(intree2, 1);
         rugged = treeCopy(intree1, 1);
-        //printf("copied tree2\n");
     }
     // get minimal branch in reference, return leaf positions and store size in pairIdx
     leavesToTransferIdx = getLeavesToTransfer(reference, name1, name2, pairIdx);
@@ -449,7 +365,6 @@ Tree* fuseTrees(Tree* intree1, Tree* intree2)
         {
             if (leavesToTransferIdx[j] == i)
             {
-                //printf("found leaf of index %d\n", i);
                 found = 1;
                 break;
             }
@@ -458,29 +373,24 @@ Tree* fuseTrees(Tree* intree1, Tree* intree2)
         {
             leavesToKeepIdx[k] = i;
             k++;
-            //printf("k increments\n");
         }
     }
-    //printf("pairIdx: %d from reference, %d from rugged\n", pairIdx[0], pairIdx[1]);
+    
     if (k != pairIdx[1]) 
     {
         fprintf(stderr, "ERROR: number of leaves to keep in rugged was not estimated properly! %d %d \n", k, pairIdx[1]); 
     }
     // randomly select fuse point
-    //srand(time(NULL));
     k = rand() % pairIdx[1];
     found = leavesToKeepIdx[k];
 
     leavesToTransfer = (char**)calloc(sizeof(char*), pairIdx[0] + 1);
     leavesToKeep = (char**)calloc(sizeof(char*), pairIdx[1]);
     
-    //printf("leaves to transfer:\t");
     for (i = 0; i < pairIdx[0]; i++)
     {
         leavesToTransfer[i] = reference->leaves[leavesToTransferIdx[i]]->name;
-        //printf("%s\t", leavesToTransfer[i]);
     }
-    //printf("\n");
     
     leavesToTransfer[pairIdx[0]] = reference->leaves[found]->name; //!!!
     for (i = 0; i < pairIdx[1]; i++)
@@ -491,8 +401,6 @@ Tree* fuseTrees(Tree* intree1, Tree* intree2)
     tmp1 = treePrune(reference, leavesToTransfer, pairIdx[0] + 1, 1);
     tmp2 = treePrune(rugged, leavesToKeep, pairIdx[1], 1);
 
-    //printf("pruned reference:\n%s\n", treeToString(tmp1));
-    //printf("pruned rugged:\n%s\n", treeToString(tmp2));
     // fuse trees:
     Node* curnode;
     Node* nei;
@@ -505,14 +413,12 @@ Tree* fuseTrees(Tree* intree1, Tree* intree2)
     tmp2->nodes[tmp2->nodesNum] = curnode;
     curnode->pos = tmp2->nodesNum;
     tmp2->nodesNum++;
-    //printf("%d nodes in result\n", tmp2->nodesNum);
     // select leaf found by name and link to newnode
     // select leaf's nei from tmp2 and link to it
     for (i = 0; i < tmp2->leavesNum; i ++)
     {
         if (strcmp(tmp2->leaves[i]->name, reference->leaves[found]->name) == 0)
         {
-            //printf("found leaf of name %s with %d neighbours\n", tmp2->leaves[i]->name, tmp2->leaves[i]->neiNum);
             nei = tmp2->leaves[i]->neighbours[0];
             for (j = 0; j < nei->neiNum; j ++)
             {
@@ -547,7 +453,6 @@ Tree* fuseTrees(Tree* intree1, Tree* intree2)
             break;
         }
     }
-    //printf("curnode has %d neighbours\n", curnode->neiNum);
     // for node in tmp1: push node to tmp2 list, 
     // rebind pos, if leaf -- do same
     for (i = 0; i < tmp1->nodesNum; i++)
@@ -565,7 +470,6 @@ Tree* fuseTrees(Tree* intree1, Tree* intree2)
             }
         }
     }
-    //printf("result now has %d nodes and %d leaves\n", tmp2->nodesNum, tmp2->leavesNum);
     // remove tmp1 manually, not deleting nodes themselves
     nodeDelete(leaf); // we only need to delete this leaf here?
     free(tmp1->nodes);
@@ -575,11 +479,10 @@ Tree* fuseTrees(Tree* intree1, Tree* intree2)
         lcaFinderDelete(tmp1->lcaFinder);
     }
     free(tmp1);
-    //printf("tmp1 deleted\n");
     // copy tmp2 to result
     result =  treeCopy(tmp2, 0);
     treeLCAFinderCalculate(result);
-    // free memory 
+    // free arrays
     free(permutation);
     free(pairIdx);
     free(leavesToKeepIdx);
@@ -593,12 +496,10 @@ Tree* fuseTrees(Tree* intree1, Tree* intree2)
     free(dist1);
     free(dist2);
     free(relDist);
-    //printf("arrays deleted\n");
-    //free reference, rugged, tmp2
+    // free reference, rugged, tmp2
     treeDelete(reference);
     treeDelete(rugged);
     treeDelete(tmp2);
-    //printf("all intermediate trees deleted\n");
     return result;
 } //fuseTrees
 
